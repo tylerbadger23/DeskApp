0348bfba744f1d78a18fd95125103a4213ad4723
tylerbadger23 <31893030+tylerbadger23@users.noreply.github.com>
2020-03-24 11:06:21 -0600

    Fixed issue with OSX copy and paste and command btn not working

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 const { app, BrowserWindow, Menu } = require('electron');
 const path = require('path');
+let Datastore = require("nedb");
+let settingsDb = new Datastore("settings.db");
+settingsDb.loadDatabase();
 
+let startupTimer = 7000;
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
   app.quit();
 }
 
 const createWindow = () => {
+
   // Create the browser window.
   const mainWindow = new BrowserWindow({
     webPreferences :{
@@ -18,7 +23,7 @@ const createWindow = () => {
     height: 900,
     frame: true
   });
-
+  
   const bootWindow = new BrowserWindow({
     webPreferences :{
       nodeIntegration: true
@@ -30,12 +35,13 @@ const createWindow = () => {
     frame: false
   });
 
+
+  mainWindow.loadFile(path.join(__dirname, '/pages/landing.html'));
   mainWindow.hide();
   setTimeout(() => {
-    mainWindow.loadFile(path.join(__dirname, '/pages/landing.html'));
     bootWindow.close();
     mainWindow.show();
-  }, 8000)
+  }, startupTimer);
 
   
   // and load the index.html of the app.
@@ -45,11 +51,32 @@ const createWindow = () => {
   //mainWindow.webContents.openDevTools();
 };
 
+    // Create the Application's main menu
+    var template = [{
+      label: "Application",
+      submenu: [
+          { label: "About", selector: "orderFrontStandardAboutPanel:" },
+          { type: "separator" },
+          { label: "Quit", accelerator: "Command+Q", click: function() { app.quit(); }}
+      ]}, {
+      label: "Edit",
+      submenu: [
+          { label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
+          { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
+          { type: "separator" },
+          { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
+          { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
+          { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
+          { label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
+      ]}
+  ];
+
+  Menu.setApplicationMenu(Menu.buildFromTemplate(template));
+
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.on('ready', createWindow);
-Menu.setApplicationMenu(null)
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
   // On OS X it is common for applications and their menu bar
@@ -69,3 +96,5 @@ app.on('activate', () => {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and import them here.
+
+
