bfa68f20b23d51f45f909192313873204b23051f
tylerbadger23 <31893030+tylerbadger23@users.noreply.github.com>
2020-03-26 01:57:56 -0700

    Fixed Navigationbar

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ const { app, BrowserWindow, Menu } = require('electron');
 const path = require('path');
 let Datastore = require("nedb");
 
-
+//cnstant timer for all startupos
+//should be waiting for promise returned aftyer checking pries
 let startupTimer = 7500;
-// Handle creating/removing shortcuts on Windows when installing/uninstalling.
+
 if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
   app.quit();
 }
@@ -60,14 +61,13 @@ const createWindow = () => {
     
   }, startupTimer);
 
-  // and load the index.html of the app.
   bootWindow.loadFile(path.join(__dirname, '/pages/splash.html'));
 
   // Open the DevTools.
   //mainWindow.webContents.openDevTools();
 };
 
-    // Create the Application's main menu
+    // Create the main menu
     var template = [{
       label: "Application",
       submenu: [
@@ -89,28 +89,21 @@ const createWindow = () => {
 
   Menu.setApplicationMenu(Menu.buildFromTemplate(template));
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
+
 app.on('ready', createWindow);
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
     app.quit();
   }
 });
 
 app.on('activate', () => {
-  // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (BrowserWindow.getAllWindows().length === 0) {
     createWindow();
   }
 });
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and import them here.
 
 
---
+++ x64-windowsBuild/readme.txt
@@ -0,0 +1,14 @@
+Developed by: Tyler Laceby
+About me: College student and passionate programmer. 
+
+NOTES || Disclaimers: 
+This application is in a very unstable state and may have major bugs that can crash the application at any time. This app is a hobby project and is not at a production ready state.
+
+If you would like to contribute to this app or contact me, please send me a message at:
+  https://github.com/tylerbadger23/
+  https://twitter.com/tylerbadger23
+  https://www.instagram.com/tylerbadger23/
+
+
+Description: Amazon price checker and notification platform developed for Windowsa and OSX computers. 
+
