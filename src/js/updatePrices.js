7acfaca0eac5d0fb9fb1fbd68978dc65f2e6ac50
Tyler Laceby <31893030+tylerbadger23@users.noreply.github.com>
2020-05-11 22:17:46 -0600

    Fixed passive updating null err

--- src/js/updatePrices.js
+++ src/js/updatePrices.js
@@ -2,7 +2,7 @@ let cheerio = require('cheerio');
 let request = require('request');
 let fs = require("fs");
 let prodInterval = 180000;
-let devInterval = 7000;
+let devInterval = 10000;
 
 async function updateProduct(id, url, price, numChecks, title, alertSettings, cheapestEverPrice, isActive, wantsAlerts) {
     let productIsActive = isActive;
@@ -13,18 +13,15 @@ async function updateProduct(id, url, price, numChecks, title, alertSettings, ch
             let new_price = $("#priceblock_ourprice").html();
             let new_num_checks = numChecks + 1;
 
-            if(new_num_checks < 39) {
-                new_price = null;
-            }
+           
             if(typeof new_price == "string") {
+                productIsActive = true;
                 
                 // check prices and then send notification if user is on
                 if(typeof price == "string") { // if price has not changed on update then compare prices
-                    console.log(`sent ${title}`)
                     comparePricesDifferent(cheapestEverPrice, price, new_price, title, id, database, wantsAlerts, alertSettings);
                 } else { // if price just got added then alert user accordingly
                     if(wantsAlerts) sendNotification(`Product is now available! - ${new_price}`, `${title}`);
-                    console.log(`sent ${title}`)
                 }
                 
 
@@ -40,7 +37,6 @@ async function updateProduct(id, url, price, numChecks, title, alertSettings, ch
                 console.log(`sent ${title}`)
                 if(typeof price == "string" && wantsAlerts && alertSettings == "Any Price Change") { 
                     sendNotification(`Product is now available! - ${new_price}`, `${title}`);
-                    console.log(`sent ${title}`)
                 }
                 productIsActive = false; // product is not active
                 await database.update({_id: id }, { $set: { price: new_price } }, {multi:true}, function (err, numReplaced) {
@@ -70,7 +66,7 @@ async function updateProduct(id, url, price, numChecks, title, alertSettings, ch
 }
 
 async function startUpdating () { //get all products crwaled
-    await database.loadDatabase((err)=> {
+    await database.loadDatabase((err) => {
         if(!err) {
             console.log(`Database loaded`);
         } else {
@@ -95,12 +91,11 @@ async function startUpdating () { //get all products crwaled
 let updateInterval = devInterval; //interval for updating data
 
 setTimeout(()=> {
-    startUpdating();
     setInterval(()=> { //update db after every x miliseconds
         startUpdating();
         console.log("Init started");
-    }, updateInterval + 20000);
-}, 1000);
+    }, updateInterval);
+}, 2000);
 
 function sendNotification(header, msg) {
     let myNotification = new Notification(header.toString(), {
